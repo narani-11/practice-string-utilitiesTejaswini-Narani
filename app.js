17d71f5105d004b15d50db7122bf9038c3a0e299
narani-11 <naranitejaswini29@gmail.com>
2025-09-03 22:01:34 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+//import the functions from stringutilities.js
+
+const stringutilities=require('./stringUtilities.js')
+
+//capitalize the first index[0]
+stringutilities.capitalize("viradhya");
+
+//reverse the string
+stringutilities.reverse("program");
+
+//contains substring in string
+stringutilities.contains("launchcode","code");
