81f9598303ab67a1e558095bed9686226e2bbd39
narani-11 <naranitejaswini29@gmail.com>
2025-09-11 21:40:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 //import the functions from stringutilities.js
 
-const stringutilities=require('./stringUtilities.js')
-
-//capitalize the first index[0]
-stringutilities.capitalize("viradhya");
-
-//reverse the string
-stringutilities.reverse("program");
-
-//contains substring in string
-stringutilities.contains("launchcode","code");
+const stringUtils = require('./stringUtils');
+// Test capitalize
+console.log("Capitalize Tests:");
+console.log(stringUtils.capitalize("hello"));   // Output: Hello
+console.log(stringUtils.capitalize(""));        // Output: (empty string)
+console.log(stringUtils.capitalize(undefined)); // Output: (empty string)
+// Test reverse
+console.log("\nReverse Tests:");
+console.log(stringUtils.reverse("hello"));   // Output: olleh
+console.log(stringUtils.reverse("JavaScript")); // Output: tpircSavaJ
+console.log(stringUtils.reverse(""));        // Output: (empty string)
+// Test contains
+console.log("\nContains Tests:");
+console.log(stringUtils.contains("hello world", "world")); // Output: true
+console.log(stringUtils.contains("hello world", "js"));    // Output: false
+console.log(stringUtils.contains("", "a"));                // Output: false
+console.log(stringUtils.contains("test", ""));             // Output: true
